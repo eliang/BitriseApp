571a84827f5adb20ab794e5282920344ec992827
Edwin Liang <edwin@swiftmedical.io>
2020-07-09 00:26:55 -0400

    Initial Commit

---
+++ BitriseApp/BitriseApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BitriseApp
+//
+//  Created by Edwin Liang on 2020-07-09.
+//  Copyright © 2020 Swift Medical. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
